f14cd6f6014ce3fb0f09c19b7c1b20bce6c78c94
accuracy-eminent <145879231+accuracy-eminent@users.noreply.github.com>
2025-03-01 16:42:57 -0600

    Add initial plot to shiny, add latitude and longitude controls

--- app.R
+++ app.R
@@ -6,13 +6,22 @@ ui <- fluidPage(
   p(),
   titlePanel("Species Distribution Model"),
   textInput("species_name","Species name","Taraxacum officinale"),
-  actionButton("calc","Calculate distribution")
+  numericInput("lat","Location latitude", 0.00, min=-90, max=90),
+  numericInput("lon","Location longitude", 0.00, min=-180, max=180),
+  actionButton("calc","Calculate distribution"),
+  plotOutput("plot")
 )
 
 server <- function(input, output, session){
   create_map <- function(){
     clim_species_data <- clim_and_species(clim_data, input$species_name)
-    print(clim_species_data %>% head())
+    combined_data <- merge_out %>% 
+      pivot_longer(cols=starts_with("bio_"))
+    output$plot <- renderPlot({
+      ggplot(combined_data %>% filter(grepl("bio_[0-9]+$",name))) +
+      geom_boxplot(aes(x=name,y=value)) +
+      theme_bw()
+    })
   }
   p <- observeEvent(input$calc, {create_map()}, ignoreNULL = FALSE)
 }
