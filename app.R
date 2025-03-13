4406298b8c0867c79431ec067ed119f39ce7ef41
accuracy-eminent <145879231+accuracy-eminent@users.noreply.github.com>
2025-03-12 21:24:06 -0500

    Add mean absolute deviation

--- app.R
+++ app.R
@@ -9,6 +9,7 @@ ui <- fluidPage(
   numericInput("lat","Location latitude", 35.00, min=-90, max=90),
   numericInput("lon","Location longitude", -80.00, min=-180, max=180),
   actionButton("calc","Calculate distribution"),
+  textOutput("suitability"),
   tableOutput("table"),
   plotOutput("plot")
 )
@@ -31,8 +32,11 @@ server <- function(input, output, session){
       mutate(species_sd=sd(clim_species_data[[name]])) %>%
       ungroup() %>%
       mutate(z=(value - species_mean)/species_sd) %>%
-      select(name, value, species_mean, species_sd, z)
+      select(name, value, species_mean, species_sd, z) %>%
+      rename(var_name=name, loc_value=value, z_score=z)
     output$table <- renderTable(suitability_df, striped=TRUE)
+    # Show suitability summary
+    output$suitability <- renderText(sprintf("Mean absolute suitability: %s", mean(abs(suitability_df$z_score))))
   }
   p <- observeEvent(input$calc, {create_map()}, ignoreNULL = FALSE)
 }
