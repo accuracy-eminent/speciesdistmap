0c5bca1e02f3572e48e6618a1c53494ba4ef62aa
accuracy-eminent <145879231+accuracy-eminent@users.noreply.github.com>
2025-03-15 15:52:07 -0500

    Show z-scores as perecentiles

--- app.R
+++ app.R
@@ -43,10 +43,15 @@ server <- function(input, output, session){
       rename(var_name=name, loc_value=value, z_score=z)
     output$table <- renderTable(suitability_df, striped=TRUE)
     # Show suitability summary
-    output$mean_suitability <- renderText(sprintf("Mean absolute suitability deviation: %.2f", mean(abs(suitability_df$z_score))))
-    output$max_suitability <- renderText(sprintf("Maximum absolute suitability deviation: %.2f",  mean(max(suitability_df$z_score))))
-    weighted_suitability <- (suitability_df %>% mutate(z_abs=abs(z_score)) %>% summarize(result=weighted.mean(z_abs, w=var_importance)))$result
-    output$weighted_suitability <- renderText(sprintf("Weighted mean absolute suitability deviation: %.2f", weighted_suitability))
+    mean_s_z <- mean(abs(suitability_df$z_score))
+    mean_s_p <- pnorm(mean_s_z, lower.tail=FALSE)
+    output$mean_suitability <- renderText(sprintf("Mean absolute suitability deviation: %.2f, %%ile: %.2f%%", mean_s_z, mean_s_p*100))
+    max_s_z <- mean(max(suitability_df$z_score))
+    max_s_p <- pnorm(max_s_z, lower.tail=FALSE)
+    output$max_suitability <- renderText(sprintf("Maximum absolute suitability deviation: %.2f, %%ile: %.2f%%",  max_s_z, max_s_p*100))
+    weight_s_z <- (suitability_df %>% mutate(z_abs=abs(z_score)) %>% summarize(result=weighted.mean(z_abs, w=var_importance)))$result
+    weight_s_p <- pnorm(weight_s_z, lower.tail=FALSE)
+    output$weighted_suitability <- renderText(sprintf("Weighted mean absolute suitability deviation: %.2f, %%ile: %.2f%%", weight_s_z, weight_s_p*100))
   }
   p <- observeEvent(input$calc, {create_map()}, ignoreNULL = FALSE)
 }
